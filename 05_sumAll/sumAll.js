80615d024f19bbc839320136c45057eee2224443
armwx <32905298+armwx@users.noreply.github.com>
2024-07-29 16:52:50 +0500

    05 Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start,finish) {
+sum = 0; 
+if (!Number.isInteger(start) || !Number.isInteger(finish) || start <= 0 || finish <= 0) {
+    return 'ERROR';
+}
+// const sumList = []; 
 
+// declaring new values to satisfy tests where the greater value is first (start), and smaller second (finish) in parameter
+let a = start;
+let b = finish;
+
+if (start > finish) {
+    a = finish;
+    b = start;
+}
+
+for (let i = a; i <= b; i++){
+    //sumList.push(i);
+    sum += i;
+}
+//console.log(`the sum list is: ${sumList}`)
+return sum;
 };
+
+// console.log(sumAll(5.5,1));
 
 // Do not edit below this line
 module.exports = sumAll;
