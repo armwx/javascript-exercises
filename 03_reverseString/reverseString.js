024b872c35a9efdd06e77117e38be03bb32afef1
armwx <32905298+armwx@users.noreply.github.com>
2024-07-29 12:53:04 +0500

    Solved 03
    Had some issues with initiation of functions and calling them. Also need to confim naming in future exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,27 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+
+    const convert2Array = function(word) {
+        return word.split(''); }
+
+    const reverseArray = function(array){
+        return array.reverse(); }
+
+    const joinArray = function (array) {
+        return array.join(''); }
+
+    const wordArray = convert2Array(word);
+
+    const reversedArray = reverseArray(wordArray);
+
+    const reversedString = joinArray(reversedArray)
+
+    return reversedString;
 
 };
+
+// console.log(reverseString('hello world'));
+
 
 // Do not edit below this line
 module.exports = reverseString;
