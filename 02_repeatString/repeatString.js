89b2978a603070c1981185df2301eeb6017d84f4
armwx <32905298+armwx@users.noreply.github.com>
2024-07-29 12:01:53 +0500

    Solved 02
    Solved using 2 different loops.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+let print = 1;
+let output = "";
+if (number < 0) {
+    return 'ERROR';
+}
 
+// Completed this task using 2 dif loops. 
+
+for(let i =0; i < number; i++){
+    output += word;
+ }
+
+//while (number >= print) {
+//    output += word;
+//   print++; };
+
+
+return output;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
