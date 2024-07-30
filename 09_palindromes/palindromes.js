3c6b3fd26cc0757fc9d508ba1c75fc84732002ab
armwx <32905298+armwx@users.noreply.github.com>
2024-07-30 16:26:16 +0500

    09 Solved.
    Logic was gg. Had to look into replacing spaces etc with nada.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (input) {
+const word = input.toLowerCase().replace(/[.,!?\s]/g, '');
+const wordArray = word.split('');
+const wordArrayReverse = wordArray.slice().reverse();
+
+const result = wordArray.every((value,index) => value === wordArrayReverse[index]);
+
+return result;
+
+// return `
+// Result: ${result}
+// ${typeof(word)}
+// Word: ${word}
+
+// ${typeof(wordArray)}
+// Array: ${wordArray} 
+
+// ${typeof(wordArrayReverse)}
+// Reverse: ${wordArrayReverse}`
 
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
