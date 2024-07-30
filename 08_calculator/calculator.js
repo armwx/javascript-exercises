1e95a388cfbe4c1578d73d249c92e8fbdc7f646a
armwx <32905298+armwx@users.noreply.github.com>
2024-07-30 12:15:21 +0500

    08 Solved.
    Had some weird issues with sum and multiply. Documented them to investigate later.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
+};
+const sum = function(arr) {
+  return arr.reduce((acc, val) => acc + val, 0);
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  return arr.reduce((acc, val) => acc * val);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const power = function() {
-	
-};
+const factorial = function(n) {
+    if (n < 0) {
+        return 'ERR'; // Factorial is not defined for negative arr
+    }
+    if (n === 0 || n === 1) {
+        return 1; // Factorial of 0 and 1 is 1
+    }
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
