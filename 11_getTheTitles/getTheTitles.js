b477f29720ec38332c59aa67d69b0fadf22d66ea
armwx <32905298+armwx@users.noreply.github.com>
2024-07-30 17:49:15 +0500

    11 Solved.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    console.log(typeof books)
+    console.log(books.length)
+    
+    console.log(books)
+    const bookTitles = [];
+    for (const book of books) {
+        const name = book.title;
+        bookTitles.push(name);
+    }
+    return bookTitles;
 };
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
+
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
