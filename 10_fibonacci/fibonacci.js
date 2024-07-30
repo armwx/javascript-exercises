7b74213e29390b3b51dcca268e1f284b26dad014
armwx <32905298+armwx@users.noreply.github.com>
2024-07-30 17:05:32 +0500

    10 Solved.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const fibonacci = [1,1];
+    if (n < 0){
+        return "OOPS"
+    } else if (n == 0) {
+        return 0;
+    }
+    for (i=2; i <= n; i++) {
+        const next = fibonacci[i-1] + fibonacci[i-2];
+        fibonacci.push(next);
+    }
+    return parseInt(fibonacci[n-1])
+    // `answer: ${parseInt(fibonacci[n-1])}
+    // array: ${fibonacci}`
 };
 
+console.log(fibonacci(0));
 // Do not edit below this line
 module.exports = fibonacci;
