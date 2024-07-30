7e7d18124057ac6fff5ee02b876b3f4aa123704f
armwx <32905298+armwx@users.noreply.github.com>
2024-07-30 10:36:54 +0500

    07 Solved
    Used Float and .toFixed to convert string to a 1DP number.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let minus = tempF - 32
+  let tempC = minus / 1.8;
+  return parseFloat(tempC.toFixed(1));
+  // https://www.calculatorsoup.com/calculators/conversions/fahrenheit-to-celsius.php
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let ninebyfive = 9/5;
+  let tempF = tempC * ninebyfive + 32
+  return parseFloat(tempF.toFixed(1));
+  //https://www.calculatorsoup.com/calculators/conversions/celsius-to-fahrenheit.php
+
 };
+
+// number.toFixed(decimal_digits);
+// parseFloat(number.toFixed(decimal_digits);)
 
 // Do not edit below this line
 module.exports = {
